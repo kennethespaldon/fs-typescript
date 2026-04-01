78e781e24be5f9f21bfd9de7b124a228bada0899
Kenneth Espaldon <kenespaldon@gmail.com>
2026-03-31 23:16:58 -0700

    feat: add HTTP GET endpoint "bmi"

--- healthapp/index.ts
+++ healthapp/index.ts
@@ -1,8 +1,27 @@
+import { bmiCalculator } from './bmiCalculator.ts';
+import { isNotNumber } from './utils.ts';
 import express from 'express';
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  if (isNotNumber(req.query.height) || isNotNumber(req.query.weight)) {
+    res.status(400).send({ error: 'malformatted errors' });
+  }
+  
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+
+  const bmi = bmiCalculator(height, weight);
+
+  res.send({
+    weight,
+    height,
+    bmi
+  })
 });
 
 const PORT = 3001;
