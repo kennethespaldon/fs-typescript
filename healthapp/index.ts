5c98ed8c3efbf460435fed7703ac7118d5b2a1e8
Kenneth Espaldon <kenespaldon@gmail.com>
2026-04-03 22:29:04 -0700

    feat: add HTTP POST endpoint "exercises"

--- healthapp/index.ts
+++ healthapp/index.ts
@@ -1,7 +1,10 @@
 import { bmiCalculator } from './bmiCalculator.ts';
+import { calculateExercises } from './exerciseCalculator.ts';
 import { isNotNumber } from './utils.ts';
 import express from 'express';
 const app = express();
+
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
@@ -9,7 +12,7 @@ app.get('/hello', (_req, res) => {
 
 app.get('/bmi', (req, res) => {
   if (isNotNumber(req.query.height as string) || isNotNumber(req.query.weight as string)) {
-    res.status(400).send({ error: 'malformatted errors' });
+    res.status(400).send({ error: 'malformatted parameters' });
   }
   
   const height = Number(req.query.height);
@@ -24,7 +27,34 @@ app.get('/bmi', (req, res) => {
   });
 });
 
-const PORT = 3001;
+app.post('/exercises', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+  const hours = req.body.daily_exercises;
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+  const target = req.body.target;
+
+  if (hours === undefined || target === undefined) {
+    res.status(400).send({ error: 'parameters missing' });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+  hours.forEach((hour) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    if (isNotNumber(hour)) {
+      res.status(400).send({ error: 'malformatted parameters' });
+    }
+  });
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  if (isNotNumber(target)) {
+    res.status(400).send({ error: 'malformatted parameters' });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  return res.send(calculateExercises(hours, target));
+});
+
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
